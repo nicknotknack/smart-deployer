f596b8469f3859b14c96bee09f8f94b246131cf0
Nick <nick.kolya.1998@gmail.com>
2025-04-30 16:55:46 +0400

    feat: comment for ERC20Airdroper added

--- src/ERC20Airdroper/ERC20Airdroper.sol
+++ src/ERC20Airdroper/ERC20Airdroper.sol
@@ -5,32 +5,48 @@ import "../UtilityContract/AbstractUtilityContract.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-/// @title ERC20Airdroper - Airdrop utility contract for ERC20 tokens
+/// @title ERC20Airdroper - Utility contract for batch ERC20 token distribution
 /// @author Solidity University
-/// @notice This contract allows the owner to airdrop ERC20 tokens to multiple addresses.
-/// @dev Inherits from AbstractUtilityContract and Ownable
+/// @notice Enables owners to airdrop ERC20 tokens to multiple recipients
 contract ERC20Airdroper is AbstractUtilityContract, Ownable {
+    /// @notice Initializes Ownable with deployer
     constructor() payable Ownable(msg.sender) {}
 
+    /// @notice Maximum number of token transfers per airdrop call
     uint256 public constant MAX_AIRDROP_BATCH_SIZE = 300;
 
+    /// @notice ERC20 token contract to distribute from
     IERC20 public token;
+
+    /// @notice Fixed amount used for allowance check
     uint256 public amount;
+
+    /// @notice Address holding tokens to be distributed
     address public treasury;
 
+    /// @dev Reverts if contract already initialized
     error AlreadyInitialized();
+    /// @dev Reverts if receivers and amounts array lengths mismatch
     error ArraysLengthMismatch();
+    /// @dev Reverts if insufficient token allowance from treasury
     error NotEnoughApprovedTokens();
+    /// @dev Reverts if ERC20 transfer fails
     error TransferFailed();
+    /// @dev Reverts if batch size exceeds limit
     error BatchSizeExceeded();
 
+    /// @dev Restricts `initialize` to one-time execution
     modifier notInitialized() {
         require(!initialized, AlreadyInitialized());
         _;
     }
 
+    /// @dev Tracks initialization status
     bool private initialized;
 
+    /// @notice Distributes ERC20 tokens from treasury to recipients
+    /// @param receivers Addresses to receive tokens
+    /// @param amounts Amount of tokens to send per recipient
     function airdrop(address[] calldata receivers, uint256[] calldata amounts) external onlyOwner {
         require(receivers.length <= MAX_AIRDROP_BATCH_SIZE, BatchSizeExceeded());
         require(receivers.length == amounts.length, ArraysLengthMismatch());
@@ -46,6 +62,10 @@ contract ERC20Airdroper is AbstractUtilityContract, Ownable {
         }
     }
 
+    /// @inheritdoc IUtilityContract
+    /// @notice Initializes the airdropper contract with required config
+    /// @param _initData Encoded deployManager, token, amount, treasury, and new owner
+    /// @return success True if initialized
     function initialize(bytes memory _initData) external override notInitialized returns (bool) {
         (address _deployManager, address _token, uint256 _amount, address _treasury, address _owner) =
             abi.decode(_initData, (address, address, uint256, address, address));
@@ -62,6 +82,13 @@ contract ERC20Airdroper is AbstractUtilityContract, Ownable {
         return true;
     }
 
+    /// @notice Helper to encode constructor-style init data
+    /// @param _deployManager Address of the DeployManager
+    /// @param _token Address of ERC20 token contract
+    /// @param _amount Amount used to validate allowance
+    /// @param _treasury Address holding the tokens
+    /// @param _owner New owner of the contract
+    /// @return Encoded initialization bytes
     function getInitData(address _deployManager, address _token, uint256 _amount, address _treasury, address _owner)
         external
         pure
@@ -70,6 +97,3 @@ contract ERC20Airdroper is AbstractUtilityContract, Ownable {
         return abi.encode(_deployManager, _token, _amount, _treasury, _owner);
     }
 }
-
-//["0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2","0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db","0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB","0x617F2E2fD72FD9D5503197092aC168c91465E7f2"]
-//[2500000000000000000000,3100000000000000000000,1900000000000000000000,2500000000000000000000]
